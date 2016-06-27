e5f9ddac547e28fe1537e8a781587d6687c71200
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-06-27 19:59:45 -0400

    Refactoring code and help of group.wise.sample.size

--- R/group.wise.sample.size.R
+++ R/group.wise.sample.size.R
@@ -1,20 +1,22 @@
 #' group.wise.sample.size
 #'
-#' Utility function to keep from having to pass group-wise sample sizes between functions.
+#' Utility function to avoid passing group-wise sample sizes between functions.
 #' @author Edmund R Glass, \email{Edmund.Glass@@gmail.com}
 #' @references \url{https://github.com/ERGlass/lrcde.dev}
-#' @param groups  A vector of 1's and 2's indicating group membership per sample.  Should align with samples in het.sub and cell.props.
-#' @return n  A vector containing control and cases sample sizes.
+#' @param groups A vector of identifiers indicating group membership per sample.
+#' Should have the same length as the number of rows in het.sub and cell.props.
+#' @return n  A vector containing group-wise sample sizes.
 #' @keywords Deconvolution cell type-specific differential expression detection power analysis
-#' @export
 #' @examples
-#' group.wise.sample.size( groups )
+#' \dontrun{
+#' group.wise.sample.size(group = rep(1:3, each = 5)) # Three groups, 5 samples each
+#' }
 
-group.wise.sample.size <- function( groups ){
-  unique.groups = unique( groups )
-  n = vector()
-  for( i in unique.groups) { n[i] = length( groups[ groups == i ] ) }
-  # n.controls = n[1]
-  # n.cases    = n[2]
+group.wise.sample.size <- function(groups) {
+  unique.groups <- unique(groups)
+  n <- vector(mode = "numeric", length = length(unique.groups))
+  for (i in unique.groups) {
+    n[i] <- length(groups[groups == i])
+  }
   return(n)
 }
