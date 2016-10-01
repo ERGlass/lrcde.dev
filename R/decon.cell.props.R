ed4148aa4ba759479bb7c48c8513e04d6086312e
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-10-01 12:59:52 -0400

    clarify help

--- R/decon.cell.props.R
+++ R/decon.cell.props.R
@@ -1,17 +1,14 @@
 #' decon.cell.props
 #'
-#' Call this function to estimate cell proportions given hetergenous expressions and cell signatures.
-#' @param het.sub Heterogeneous gene expression matrix, _n_ samples by _m_ genes 
+#' Function to estimate cell proportions given hetergenous gene expression matrix and 
+#' cell type-specific gene expression signatures.
+#' @param het.sub heterogeneous gene expression matrix, _n_ samples by _m_ genes 
 #' (rows by columns). Required. Note that only a subset of heterogeneous measres
 #' is used, with the same number and order of genes as in the cell signature matrix
-#' @param cell.sigs Cell-specific gene expression signatures, _p_ cell types by 
+#' @param cell.sigs cell-specific gene expression signatures, _p_ cell types by 
 #' _m_ genes (rows by columns). Required. Note that both heterogeneous matrix and 
-#' cell signature matrix should be on the same scale, log2 or decimal
-#' @return Estimated cell proportions, _n_ samples by _m_ cell types (rows by columns)
-
-#' @details
-#' decon.cell.props is an auxillary function meant to be called directly by user in order to estimate cell proportions given cell gene signatures.
-#' This assumes that G and cell.sigs have been previously made conformal with identical gene names/labels.
+#' cell signature matrix should be on the same scale, e.g. log2 or decimal
+#' @return estimated cell proportions, _n_ samples by _m_ cell types (rows by columns)
 #' @author Edmund R Glass, \email{Edmund.Glass@@gmail.com}, Mikhail G Dozmorov, \email{Mikhail.Dozmorov@@vcuhealth.org}
 #' @references \url{https://github.com/ERGlass/lrcde.dev}
 #' @keywords Deconvolution cell type-specific differential expression detection power analysis
@@ -21,7 +18,7 @@
 #' 
 #' \dontrun{
 #' # Loads the heterogeneous matrix (subset) and cell signatures
-#' data("lrcde.demo") # Included in the LRCDE function
+#' data("lrcde.demo") # Included in the LRCDE package
 #' # Estimate cell proportions. 
 #' cell.props <- decon.cell.props(het.mtx, cell.sigs)
 #' }
